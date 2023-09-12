a181eaa7b09dcf0a23d3c3fccc691e9e2a2cbe04
Leo Projceski <126206757+Leo-Proj@users.noreply.github.com>
2023-09-12 10:33:49 +1000

    update 13

--- 10.servoMotor/functions.ino
+++ 10.servoMotor/functions.ino
@@ -1,16 +1,26 @@
 void openGate () {
-  for (int i = 0; i <= 255; i++) {
-  myServo.write(i);
-  delay(45);
-  }
+
+    for (int i = 0; i <= 255; i++) {
+      myServo.write(i);
+      delay(15);
+      isGateOpen = true;
+    }
+
 }
 
 void closeGate () {
-  myServo.write(0);
-  delay(1000);
+
+    for (int i = 0; i <= 255; i++) {
+      myServo.write(255-i);
+      delay(15);
+      isGateOpen = false;
+    }
+
 }
 
-void readUS () {
-  rangeInCM = myUSsensor.MeasureInCentimeters();
+
+unsigned long readUS() {
+  unsigned long rangeInCM = myUSsensor.MeasureInCentimeters();
   Serial.println(rangeInCM);
+  return rangeInCM;
 }
